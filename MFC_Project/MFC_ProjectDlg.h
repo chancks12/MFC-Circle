14edc1aac4305f2d2f2a5eaa93143b972fb9ac1c
chan <hohoit0512@google.com>
2026-06-11 05:00:28 +0900

    가독성 처리

--- MFC_Project/MFC_ProjectDlg.h
+++ MFC_Project/MFC_ProjectDlg.h
@@ -1,27 +1,22 @@
-﻿
-// MFC_ProjectDlg.h: 헤더 파일
-//
-
-#pragma once
+﻿#pragma once
 
 
 // CMFCProjectDlg 대화 상자
 class CMFCProjectDlg : public CDialogEx
 {
-// 생성입니다.
+// 생성
 public:
 	CMFCProjectDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
 
-// 대화 상자 데이터입니다.
+// 대화 상자 데이터
 #ifdef AFX_DESIGN_TIME
 	enum { IDD = IDD_MFC_PROJECT_DIALOG };
 #endif
 
 	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원
 
 
-// 구현입니다.
 protected:
 	HICON m_hIcon;
 
